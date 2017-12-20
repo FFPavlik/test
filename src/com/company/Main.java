b7cc9a0f673b073c862bf739e8ac6d78f25d3905
FFpavlik <fpavlik13@gmail.com>
2017-12-20 23:33:06 +0300

    3commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("helloword");
         System.out.print("commit");
+        System.out.print("commit1");
 	// write your code here
     }
 }
